c8f58b8da40f41e98cab3109770c266d5231c1d1
Sera228 <144421841+Sera228@users.noreply.github.com>
2024-05-30 11:07:52 +0300

    Update 2.jl

--- Работы/2.jl
+++ Работы/2.jl
@@ -3,7 +3,7 @@ using HorizonSideRobots
 # Создание робота в позиции (15, 15) с включенной анимацией
 r = Robot(15, 15, animate=true)
 
-# Функция для перемещения робота вдоль левой границы
+# Функция для перемещения робота вдоль правой границы
 function gobok(r::Robot)
     while !isborder(r, HorizonSide(3))  # HorizonSide(3) соответствует движению влево
         move!(r, HorizonSide(3))
@@ -13,7 +13,7 @@ end
 # Функция для построения периметра путем размещения маркеров
 function buildp(r::Robot)
     hz = 0  # Инициализация индекса направления
-    while hz < 4  # Цикл по всем четырем направлениям (0: вправо, 1: вверх, 2: влево, 3: вниз)
+    while hz < 4  # Цикл по всем четырем направлениям (0: вверх, 1: влево, 2: ввниз, 3: вправо)
         if !isborder(r, HorizonSide(hz))
             move!(r, HorizonSide(hz))
             putmarker!(r)
