83c6a8e65fa7dcf8b37444d672321d7dea5832bc
Sera228 <144421841+Sera228@users.noreply.github.com>
2024-05-30 09:50:42 +0300

    Delete Работы directory

--- Работы/16.jl
+++
@@ -1,5 +0,0 @@
-using HorizonSideRobots
-function find_marker!(r::Robot)
-    tmp = (side::HorizonSide) -> ismarker(r)
-    spiral!( tmp, r)
-end
--- Работы/18.jl
+++
@@ -1,8 +0,0 @@
-using HorizonSideRobots
-function moveRec!(robot::Robot, side::HorizonSide)
-    if !isborder(robot, side)
-        
-        move!(robot, side)
-        moveRec!(robot, side)
-    end
-end
