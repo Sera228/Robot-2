f1f94f4a10083ee6e1a0365443b77ff3341d49c1
Sera228 <144421841+Sera228@users.noreply.github.com>
2024-06-04 16:50:11 +0300

    Update 3.jl

--- Работы/3.jl
+++ Работы/3.jl
@@ -1,49 +1,48 @@
 using HorizonSideRobots
 
-function mark_all_cells(r::Robot)
-    # Сохраняем исходное положение робота
-    initial_position = get_position(r)
-    
-    # Маркируем текущую клетку
-    putmarker!(r)
-    
-    # Поворачиваем робота налево и двигаемся, если это возможно
-    for side in (Nord, West, Sud, Ost)
-        while true
-            if !isborder(r, side)
-                move!(r, side)
-                putmarker!(r)
-            else
-                break
+# Создание робота в позиции (15, 15) с включенной анимацией
+r = Robot(15, 15, animate=true)
+
+# Функция для рисования спирали
+function spiralka(r::Robot)
+    hz = 3  # Начальное направление - вниз
+    while true
+        if !ismarker(r) && !isborder(r, HorizonSide(hz))
+            putmarker!(r)  # Поставить маркер
+            move!(r, HorizonSide(hz))  # Двигаться в направлении hz
+        else
+            if hz == 3
+                hz = 0
+            elseif hz == 0
+                hz = 1
+            elseif hz == 1
+                hz = 2
+            elseif hz == 2
+                hz = 3
+            end
+
+            if ismarker(r)
+                if hz == 3
+                    move!(r, HorizonSide(1))  # Повернуть вправо и вверх
+                    move!(r, HorizonSide(0))  # Повернуть вправо и вправо
+                    hz = 0
+                elseif hz == 0
+                    move!(r, HorizonSide(2))  # Повернуть вправо и влево
+                    move!(r, HorizonSide(1))  # Повернуть вправо и вверх
+                    hz = 1
+                elseif hz == 1
+                    move!(r, HorizonSide(3))  # Повернуть вправо и вниз
+                    move!(r, HorizonSide(2))  # Повернуть вправо и влево
+                    hz = 2
+                elseif hz == 2
+                    move!(r, HorizonSide(0))  # Повернуть вправо и вправо
+                    move!(r, HorizonSide(3))  # Повернуть вправо и вниз
+                    hz = 3
+                end
             end
         end
-        # Возвращаемся назад до границы
-        while ismarker(r)
-            move!(r, inverse_side(side))
-        end
     end
-    
-    # Возвращаемся в исходное положение
-    go_to_initial_position(r, initial_position)
 end
 
-function get_position(r::Robot)
-    # Функция для получения текущей позиции робота
-    # Должна быть реализована в модуле HorizonSideRobots
-end
-
-function inverse_side(side::HorizonSide)
-    # Функция для получения противоположного направления
-    # Должна быть реализована в модуле HorizonSideRobots
-end
-
-function go_to_initial_position(r::Robot, initial_position)
-    # Функция для возвращения робота в исходное положение
-    # Должна быть реализована в модуле HorizonSideRobots
-end
-
-# Создание робота в произвольной клетке на поле
-r = Robot(animate=true)
-
-# Вызов функции для маркировки всех клеток
-mark_all_cells(r)
+# Запуск функции для рисования спирали
+spiralka(r)
